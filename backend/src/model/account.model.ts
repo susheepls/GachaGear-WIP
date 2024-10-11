6e0d72169b89fc906f99204e9ec06538b0bb0ddf
susheepls <169642127+susheepls@users.noreply.github.com>
2024-10-12 02:12:49 +0900

    feat: set up docker environment
    with the help of filippo, i was successfully able to create a docker set up
    for this project. There are 4 containers, backend, frontend, postgres, and prisma studio.

--- backend/src/model/account.model.ts
+++ backend/src/model/account.model.ts
@@ -1,5 +1,4 @@
-import { Inventory, PrismaClient } from "@prisma/client";
-import { AccountCreateType } from "../interfaces/accountType";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
