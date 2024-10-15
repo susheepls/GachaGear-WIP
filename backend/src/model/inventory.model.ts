48ad510e4cefc3cb5cf913bacc5349407bdc15f7
susheepls <169642127+susheepls@users.noreply.github.com>
2024-10-15 15:54:53 +0900

    feat: add set random weighted values for substats when item is created
    I installed a new package where users are able to set weighted properties in an array of objects.
    The range of atk is 1-400, def is 1-30, hp is 1-50. There are 3 weighted ranges: 20% low, 60% avg, 20% high.

--- backend/src/model/inventory.model.ts
+++ backend/src/model/inventory.model.ts
@@ -1,6 +1,45 @@
 import { PrismaClient } from "@prisma/client";
+import Chooser from "random-seed-weighted-chooser";
 
 const prisma = new PrismaClient();
+
+interface SubstatWeight{ value: number, weight: number};
+
+//atk substat weighted chooser
+const returnWeightedATKSubStatValues = () => {
+    // 20% 1-100 60% 101-300 20% 301-400
+    const weightedATKSubStatValues = [
+        { value: Math.floor(Math.random() * 100) + 1, weight: 20 },
+        { value: Math.floor(Math.random() * 200) + 101, weight: 60 },
+        { value: Math.floor(Math.random()* 100) + 301, weight: 20}
+    ];
+    const chosenObjectValue = Chooser.chooseWeightedObject(weightedATKSubStatValues) as SubstatWeight;
+    return chosenObjectValue.value;
+};
+
+//def substat weighted chooser
+const returnWeightedDEFSubStatValues = () => {
+    // 20% 1-10 60% 11-20 20% 21-30
+    const weightedDEFSubStatValues = [
+        { value: Math.floor(Math.random() * 10) + 1, weight: 20 },
+        { value: Math.floor(Math.random() * 10) + 11, weight: 60 },
+        { value: Math.floor(Math.random()* 10) + 21, weight: 20}
+    ];
+    const chosenObjectValue = Chooser.chooseWeightedObject(weightedDEFSubStatValues) as SubstatWeight;
+    return chosenObjectValue.value;
+};
+
+//hp substat weighted chooser
+const returnWeightedHPSubStatValues = () => {
+    // 20% 1-20 60% 21-40 20% 41-50
+    const weightedHPSubStatValues = [
+        { value: Math.floor(Math.random() * 20) + 1, weight: 20 },
+        { value: Math.floor(Math.random() * 20) + 21, weight: 60 },
+        { value: Math.floor(Math.random()* 10) + 41, weight: 20}
+    ];
+    const chosenObjectValue = Chooser.chooseWeightedObject(weightedHPSubStatValues) as SubstatWeight;
+    return chosenObjectValue.value;
+};
 
 const InventoryModel = {
     createItem: async(accountId: number) => {
@@ -16,21 +55,21 @@ const InventoryModel = {
         //make the three substats       
         const itemSubstat1 = await prisma.itemSubstats.create({
             data: {
-                value: (Math.random() * 100) + 1,
+                value: returnWeightedATKSubStatValues(),
                 substatTypeId: 1,
                 item_id: createItemTableEntry.id
             }
         });
         const itemSubstat2 = await prisma.itemSubstats.create({
             data: {
-                value: (Math.random() * 100) + 1,
+                value: returnWeightedDEFSubStatValues(),
                 substatTypeId: 2,
                 item_id: createItemTableEntry.id,
             }
         });
         const itemSubstat3 = await prisma.itemSubstats.create({
             data: {
-                value: (Math.random() * 100) + 1,
+                value: returnWeightedHPSubStatValues(),
                 substatTypeId: 3,
                 item_id: createItemTableEntry.id,
             }
