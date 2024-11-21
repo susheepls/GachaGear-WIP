396c36858181462a89cbcda43e53bd98858f28a4
susheepls <169642127+susheepls@users.noreply.github.com>
2024-11-21 23:29:14 +0900

    feat: add some divs to rankings page
    Begin mapping of data for the top ranking people

--- frontend/src/pages/Rankings.tsx
+++ frontend/src/pages/Rankings.tsx
@@ -15,8 +15,32 @@ const Rankings = () => {
         setAllSubstatsRankings(allSubstatsRankingsData);
     }
 
+    const topThreeRankingsDiv = (characterArray: CharacterDataForRankings[] | null) => {
+        if(!characterArray) return <div>Loading...</div>;
+        console.log(Object.keys(characterArray))
+        return (
+            <div className='flex m-1 flex-col outline-dashed'>
+                <div className='mx-auto'>
+                    Top Ranking for Substats
+                </div>
+                <div id='top3-totalsubstats' className='flex flex-col'>
+                    <div className='mx-auto'>
+                        {characterArray[0].characterName}
+                    </div>
+                    <div className='flex justify-evenly text-center'>
+                        <div className='w-24'>{characterArray[1].characterName}</div>
+                        <div className='w-24'>{characterArray[2].characterName}</div>
+                    </div>
+                </div>
+            </div>
+        )
+        
+    }
+
     return (
-        <div>Rankings</div>
+        <div className='flex m-1 flex-col'>
+            {topThreeRankingsDiv(allSubstatsRankings)}
+        </div>
     )
 }
 
