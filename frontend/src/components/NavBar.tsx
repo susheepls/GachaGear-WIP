89bb9a3f6cf20c655c75dd4d90fb910157573931
susheepls <169642127+susheepls@users.noreply.github.com>
2024-11-30 21:46:49 +0900

    feat: add hamburger menu to NavBar with gsap animations
    Now the navbar only displays home at all times. If you want to see more,
    you would have to press the hamburger menu at the top right.
    Added SVG to public to use in navbar.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import { useUser } from "../middleware/UserContext";
+import gsap from "gsap";
 
 interface page {
     name: string,
@@ -11,15 +12,16 @@ interface page {
 
 const NavBar = () => {
     const { userInfo, fetchUserInfo } = useUser();
+    const [isHamburgerClicked, SetIsHamburgerClicked] = useState<boolean>(false);
     
     const token = Cookies.get('token');
     const navigate = useNavigate();
     const pages: page[] = [
-        {name: 'Home', href: '/', key: 'home'},
         {name: 'Inventory', href: userInfo ? `/${userInfo.username}/inventory` : '/login', key: 'inventory'},
         {name: 'Characters', href: '/characters', key: 'characters'},
         {name: 'Get Currency', href: '/currencyBox', key: 'currencybox'},
         {name: 'Roll Now!', href: '/gacharoll', key: 'gacharoll'},
+        {name: 'Rankings', href: '/rankings', key: 'rankings'},
     ];
 
     useEffect(() => {
@@ -29,18 +31,58 @@ const NavBar = () => {
         }
     }, [userInfo, fetchUserInfo, navigate, token]);
 
+    const handleHamburgerMenu = () => {
+        const hamburgerMenu = document.getElementById('hamburger');
+
+        if(!isHamburgerClicked) {
+            SetIsHamburgerClicked(true);
+            gsap.fromTo(hamburgerMenu,
+                {
+                    height:0
+                },
+                {
+                    height:170 //height of navbar opened
+                },
+            )
+        } else {
+            SetIsHamburgerClicked(false);
+            gsap.fromTo(hamburgerMenu,
+                {
+                    height:170
+                },
+                {
+                    height:0
+                },
+            )
+        }
+    }
+
     return (
         <nav>
-            <div className="flex bg-three text-white">
-                {pages.map((page) => (
-                    <a
-                        className="p-1 text-whiteblue"
-                        key={page.key}
-                        href={page.href}
-                    >   
-                        {page.name}
-                    </a>
+            <div className="max-h-8 flex justify-between bg-three text-white">
+                <a
+                    className="p-1 text-four"
+                    key={'home'}
+                    href={'/'}
+                >
+                    Home
+                </a>
+                <svg className="h-6 w-6 mr-1" onClick={handleHamburgerMenu}>
+                    <image height={30} width={20} href="burger-menu.svg"></image>
+                </svg>
+            </div>
+            <div id='hamburger' className="h-{50vh} bg-three z-50 absolute top-7 right-0">
+                <div className="w-screen flex flex-col">
+                {isHamburgerClicked && pages.map((page) => (
+                        <a
+                            className="p-1 text-four"
+                            key={page.key}
+                            href={page.href}
+                        >   
+                            {page.name}
+                        </a>
                 ))}
+                </div>
             </div>
         </nav>
     )
