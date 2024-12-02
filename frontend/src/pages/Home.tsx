2de365c1fa046e0e6615c72ea376b1e1c8159ee4
susheepls <169642127+susheepls@users.noreply.github.com>
2024-12-02 12:37:35 +0900

    styling: add svgs and styling to home page;
    I added some svgs and some more css on the home page to make it looks more cleaner and appealling.
    There is css for both being logged in and not logged in.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -43,31 +43,55 @@ const Home = () => {
         <div>
             {!token ? (
                 <div className='flex flex-col'>
-                    <div className='p-1 m-auto'>
-                        <button onClick={() => toLoginPage()}>Click Here to Login!</button>
+                    <div>
+                        <div className='text-center w-40 mx-auto bg-three text-four rounded-md my-2'>
+                            <button onClick={() => toLoginPage()}>Click Here to Login!</button>
+                        </div>
                     </div>
-                    <div className='m-auto'>
-                        <button onClick={() => toRankingPage()}>View Rankings</button>
+                    <div className='flex h-5 mx-auto mt-3'>
+                        <div>
+                            <button onClick={() => toRankingPage()}>View Rankings</button>
+                        </div>
+                        <svg className='w-4 h-4 my-auto ml-1 mt-1.5'>
+                            <image href='trophy.svg'></image>
+                        </svg>
                     </div>
                 </div>
             ) : (
-                <>
-                    <div>
+                <div className='flex flex-col'>
+                    <div className='text-center drop-shadow-[0_1.2px_1.2px_rgba(191,297,176,0.8)] text-three text-lg'>
                         Welcome back {userInfo ? userInfo.username : ''}
                     </div>
-                    <div>
-                        <button onClick={() => toAccountInventory()}>Check Inventory</button>
+                    <div className='flex h-9 ml-10'>
+                        <div className='mt-1'>
+                            <button onClick={() => toAccountInventory()}>Check Inventory</button>
+                        </div>
+                        <svg className='w-4 h-4 my-auto ml-1'>
+                            <image href='bag-shopping.svg'></image>
+                        </svg>
                     </div>
-                    <div>
-                        <button onClick={() => toGachaRoll()}>want to gamba?</button>
+                    <div className='flex h-5 ml-10'>
+                        <div className=''>
+                            <button onClick={() => toGachaRoll()}>Get Gear</button>
+                        </div>
+                        <svg className='w-4 h-4 my-auto ml-1 mt-2'>
+                            <image href='sword.svg'></image>
+                        </svg>
                     </div>
-                    <div>
-                        <button onClick={() => toRankingPage()}>Rankings</button>
+                    <div className='flex h-5 ml-10 mt-3'>
+                        <div>
+                            <button onClick={() => toRankingPage()}>Rankings</button>
+                        </div>
+                        <svg className='w-4 h-4 my-auto ml-1 mt-1.5'>
+                            <image href='trophy.svg'></image>
+                        </svg>
                     </div>
-                    <div>
-                        <button onClick={() => logout()}>Log Out</button>
+                    <div className='mt-2'>
+                        <div className='text-center w-16 mx-auto bg-three text-four rounded-md my-2'>
+                            <button onClick={() => logout()}>Log Out</button>
+                        </div>
                     </div>
-                </>
+                </div>
             )}
         </div>
     )
